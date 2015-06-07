d597a542378f20eed0a42000760f34725a90a0cf
Michael J. Kiernan <michaelk@pdonline.net.au>
2015-06-07 18:16:38 +0800

    Finished problem 4. Un-optimised (like nearly everything else :P).

---
+++ 004/4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  int largest = 0;
+  for (int x = 1000; x > 99; --x) {
+    for (int y = x; y > 99; --y) {
+      std::string product = std::to_string(x * y); // std >= c++11.
+      std::string reverse(product.rbegin(), product.rend());
+      if (!product.compare(reverse)) {
+        if (x * y > largest) {
+          largest = x * y;
+        }
+      }
+    }
+  }
+  std::cout << "Largest 3 digit palindrome: " << largest << ".";
+  std::cout << std::endl;
+  return 1;
+}
