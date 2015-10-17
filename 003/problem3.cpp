c01a291f1d8d1c608d77f9ecb28f9301a05490cc
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2015-10-17 18:47:35 +0800

    Fixed trial division check for primality. Pretty average, but it at least works now, albeit poorly. Added a swarm of comments.

--- 003/problem3.cpp
+++ 003/problem3.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <vector>
 
-// TODO: template argument so it doesn't need to explicitly be
-// a long long int?
+/**
+ * TODO: list reasoning for long long int (I mean, it is obvious but may
+ * as well list the specifics).
+ *
+ * Trial division reading:
+ *  - https://www.khanacademy.org/computing/computer-science/cryptography/comp-number-theory/a/trial-division
+ *  - https://en.wikipedia.org/wiki/Trial_division
+ *  - http://rosettacode.org/wiki/Primality_by_trial_division
+ */
 
 // static int limit = 10;
 
+// TODO: template argument so it doesn't need to explicitly be
+// a long long int?
 std::vector<long long int> factors(long long int findFactor) {
   std::vector<long long int> factorList;
 
@@ -19,18 +28,34 @@ std::vector<long long int> factors(long long int findFactor) {
 }
 
 /**
- * Determine if a number is prime.
+ * Determine if a number is prime (return true) or non-prime (return false).
+ *
+ * "A prime number (or a prime) is a natural number greater than 1 that has
+ * no positive divisors other than 1 and itself. A natural number greater than
+ * 1 that is not a prime number is called a composite number. For example, 5 is
+ * prime because 1 and 5 are its only positive integer factors, whereas 6 is
+ * composite because it has the divisors 2 and 3 in addition to 1 and 6."
+ *  - https://en.wikipedia.org/wiki/Prime_number
  */
 bool isPrime(long long int val) {
   bool prime = true;
-  if (!(val % 1) && !(val % val)) { // Check 1
-    for (int i = 2; i < val; ++i) {
-      if (val % i) {
-        prime = false;
-      }
-    }
-  } else {
+  if (val <= 1) {
     prime = false;
+  } else { // > 1.
+    // If: val % i == 0, then val / i == val || 1.
+    // If n is a prime, then the only value of x that should == 0, should be 1 and n.
+    // The only numbers that a prime should divide evenly into is 1 and itself.
+    if (!(val % 1) && !(val % val)) { // Check basic pre-requisites for a prime.
+      for (int i = 2; i < val; ++i) {
+        if (i != val) { // Gross - prevent redundant check..
+          if (!(val % i)) { // Check not a composite number.
+            prime = false;
+          }
+        }
+      }
+    } else {
+      prime = false;
+    }
   }
   return (prime);
 }
@@ -43,3 +68,4 @@ int main() {
     std::cout << std::endl;
   }
 }
+
