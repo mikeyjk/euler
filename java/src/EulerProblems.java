247b94ffa3364d6bc7bf8ff9946a29d99b4926c6
mikey <mikey@kiernan.tech>
2020-03-21 22:40:21 +0800

    minor java project fixup

--- java/src/EulerProblems.java
+++ java/src/EulerProblems.java
@@ -1,12 +1,12 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class EulerProblems {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EulerProblems {
 
     // Problem One - Multiples of 3 and 5
-    static int problemOneRecursive(int n, int limit, int sum) {
+    private static int problemOneRecursive(int n, int limit, int sum) {
         if (n < limit) {
             if ((n % 3) == 0 || (n % 5) == 0) {
                 sum += n;
@@ -48,7 +48,7 @@ public class EulerProblems {
         assertEquals(233168, problemOneRecursive(0, 1000, 0), "Output doesn't match known result");
     }
 
-static int recursive(int limit, int previous, int current, int sum) {
+    private static int recursive(int limit, int previous, int current, int sum) {
         int next = previous + current;
         if (next <= limit) {
             if (next % 2 == 0) {
@@ -81,15 +81,6 @@ static int recursive(int limit, int previous, int current, int sum) {
     @Test
     @DisplayName("2.2 (recursive)")
     void problemTwoRecursive() {
-        assertEquals(
-                4613732,
-                ProblemTwo.recursive(
-                        4000000,
-                        0,
-                        1,
-                        0
-                )
-        );
+        assertEquals(4613732, recursive(4000000, 0, 1, 0)        );
     }
-
 }
--- java/src/ProblemTwo.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class ProblemTwo {
-
-    }
